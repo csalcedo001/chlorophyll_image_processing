b86221e419d68bdc9b0ad673d496460221a2cc4a
César Salcedo <cesar.salcedo@utec.edu.pe>
2020-05-23 17:50:01 -0500

    Object detection with LAB color image processing

--- object_detection.py
+++ object_detection.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np
 import cv2
-import skimage
+from skimage import color
 from matplotlib import pyplot as plt
 from sklearn.cluster import KMeans
 
@@ -16,13 +16,34 @@ for directory in os.listdir("data/input"):
 
 		image = cv2.imread(input_path)
 		original = image.copy()
+
+		lab_image = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
+		lab_image_radius = np.sum(np.abs(lab_image.astype("float64"))**2,axis=-1)**(1./2)
+		lab_image = lab_image - 128
+		lab_image = np.round(lab_image * np.expand_dims(128 / lab_image_radius, axis=-1)).astype('uint8')
+		lab_image = lab_image + 128
+		lab_image_radius = np.sum(np.abs(lab_image.astype("float64"))**2,axis=-1)**(1./2)
+		lab_image[:,:,0] = 128
+		for row in range(lab_image.shape[0]):
+			for col in range(lab_image.shape[1]):
+				if lab_image_radius[row, col] < 255:
+					lab_image[row, col] = [0, 128, 128]
+		hue_image = cv2.cvtColor(lab_image, cv2.COLOR_LAB2BGR)
+		#hue_image = hue_image * 8
+		#hue_image = np.abs(hue_image - 128)
+		#hue_image = image
 		
-		gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+		gray = cv2.cvtColor(hue_image, cv2.COLOR_BGR2GRAY)
 		blurred = cv2.GaussianBlur(gray, (3, 3), 0)
 		canny = cv2.Canny(blurred, 120, 255, 1)
 		kernel = np.ones((5,5),np.uint8)
 		dilate = cv2.dilate(canny, kernel, iterations=1)
-		
+
+		cv2.imwrite("data/lab/" + directory + "/" + filename, hue_image)
+		cv2.imwrite("data/gray/" + directory + "/" + filename, gray)
+		cv2.imwrite("data/blurred/" + directory + "/" + filename, gray)
+		cv2.imwrite("data/canny/" + directory + "/" + filename, canny)
+
 		# Find contours
 		cnts = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 		cnts = cnts[0] if len(cnts) == 2 else cnts[1]
@@ -31,12 +52,15 @@ for directory in os.listdir("data/input"):
 		image_number = 0
 		for c in cnts:
 			x,y,w,h = cv2.boundingRect(c)
+
+			if w < 100 or h < 100:
+				continue
 			
 			valid_points = []
 		
 			# Evaluate only points inside ellipse
-			for row in range(y, y + h + 1):
-				for col in range(x, x + w + 1):
+			for row in range(y, y + h):
+				for col in range(x, x + w):
 					# Points to be transformed in the plane
 					y_tran = row
 					x_tran = col
@@ -75,8 +99,8 @@ for directory in os.listdir("data/input"):
 		
 			object_color = kmeans.cluster_centers_[index]
 		
-			# print("Object", image_number, "color: ", skimage.color.rgb2lab(object_color))
-			print("Object", image_number, "color: ", object_color)
+			print("Object", image_number, "color: ", color.rgb2lab(object_color))
+			# print("Object", image_number, "color: ", object_color)
 		
 			if constant_box_color:
 				box_color = (36,255,12)

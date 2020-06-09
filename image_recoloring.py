4e3e91c4b9709e676b5bbb30149b07fb8f71bd1f
César Salcedo <cesar.salcedo@utec.edu.pe>
2020-06-09 08:22:11 -0500

    Added image recoloring module with skeleton of main function

---
+++ image_recoloring.py
@@ -0,0 +1,19 @@
+def image_recoloring(target_image, target_colors, reference_colors):
+	"""
+	Recolors TARGET_IMAGE to approximate TARGET_COLORS as much as
+	possible to REFERENCE_COLORS.
+
+	Attributes:
+	target_image -- image whose colors will be updated
+	target_colors -- colors that represent the palette of target_image.
+	reference_colors -- colors to which target_colors must approximate.
+
+	Returns:
+	recolored_image -- image whose colors are as close as possible to those of reference_colors
+	"""
+
+	# TODO: Implement function
+	
+	recolored_image = target_image
+
+	return recolored_image

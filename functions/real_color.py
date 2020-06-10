e91c0483c0699b08ceac91a564d94cff0e66c0b6
César Salcedo <cesar.salcedo@utec.edu.pe>
2020-06-10 15:33:18 -0500

    Added a description for module real_color.py

--- functions/real_color.py
+++ functions/real_color.py
@@ -1,13 +1,20 @@
+"""
+Module of functions used to calculate the "real" color of an object
+from the k-means cluster created from its pixels. Each function should 
+have the following structure:
+
+Arguments:
+kmeans -- computed clusters from pixels of some image.
+
+Returns:
+object_color -- color of the biggest cluster.
+"""
+
 def biggest_cluster(kmeans):
 	"""
-	Computes color of the largest cluster.
-
-	Arguments:
-	kmeans -- computed clusters from pixels of some image.
-
-	Returns:
-	object_color -- color of the biggest cluster.
+	Select the biggest cluster's mean color as the object color.
 	"""
+
 	mean_count = [0] * len(kmeans.cluster_centers_)
 	
 	for label in kmeans.labels_:

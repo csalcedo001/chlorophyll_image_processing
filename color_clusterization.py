47666a291c41b35258f3edca7c15ddd46e0d7d99
César Salcedo <cesar.salcedo@utec.edu.pe>
2020-06-14 13:14:35 -0500

    Added color clusterization file

---
+++ color_clusterization.py
@@ -0,0 +1,46 @@
+import os
+import numpy as np
+import cv2
+from sklearn.cluster import KMeans
+from skimage import color
+import json
+
+from functions.main import detect_objects
+from functions import choose_valid_points
+
+colors = []
+
+directory = "plant_images"
+number_of_clusters = 4
+choose_valid_points = choose_valid_points.ellipse
+
+for filename in os.listdir("data/input/" + directory):
+	input_path = "data/input/" + directory + "/" + filename
+
+	print(input_path)
+
+	image = cv2.imread(input_path)
+
+	contours = detect_objects(image)
+
+	for c in contours:
+		x, y, w, h = cv2.boundingRect(c)
+
+		if w < 100 or h < 100:
+			continue
+
+		valid_points = choose_valid_points(x, y, w, h)
+	
+		# Group selected points into clusters
+		clusters = KMeans(n_clusters=number_of_clusters, random_state=0).fit(np.array([image[p[0]][p[1]] for p in valid_points]))
+
+		colors.append(clusters.cluster_centers_)
+
+rgb_colors = np.concatenate(colors, axis=0)[:,::-1]
+lab_colors = [color.rgb2lab(pixel) / 100 for pixel in rgb_colors]
+
+with open("data/lab_cluster_colors.json", "w") as output_file:
+	json.dump(np.array(lab_colors).tolist(), output_file)
+
+clusters = KMeans(n_clusters=4, random_state=0).fit(lab_colors)
+print(clusters.cluster_centers_)

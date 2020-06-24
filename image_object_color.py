91820ac468567f14ef452a1d92d0d61fbed2b516
César Salcedo <cesar.salcedo@utec.edu.pe>
2020-06-24 16:54:20 -0500

    Updated implementation of image object color choosing file

--- image_object_color.py
+++ image_object_color.py
@@ -1,27 +1,35 @@
 """
-Load image from IMAGE_PATH, detect the objects in it and print their colors.
+Load image from IMAGE_PATH, detect the objects in it and print their colors
+as COLOR_FORMAT.
 """
 
 import sys
 
-if len(sys.argv) not in [2, 3]:
-	print("Usage: python3 image_object_color.py <image_path> [<result_path>]")
-	exit()
+if len(sys.argv) not in [2, 3, 4]:
+	raise Exception("Usage: python3 image_object_color.py <image_path> [<color_format>]")
 
 import cv2
 from functions.main import *
 
+
+# Arguments
+## Image path
 image_path = sys.argv[1]
 
+## Color format
 if len(sys.argv) == 3:
-	result_path = sys.argv[2]
+	if sys.argv[2] not in ["RGB", "BGR", "LAB"]:
+		raise Exception("Unsupported color format " + str(sys.argv[2]))
+
+	color_format = sys.argv[2]
 else:
-	result_path = "result.png"
+	color_format = "LAB"
+
 
 image = cv2.imread(image_path)
 
 contours = detect_objects(image)
-colors = get_colors(image, contours, choose_color = choose_color.biggest_colored_cluster)
+colors = get_colors(image, contours, choose_color = choose_color.biggest_colored_cluster)["object_colors"]
 
-print("RGB color of objects:")
-print([object_color_data["color"].tolist() for object_color_data in colors["object_colors"]])
+for color in colors:
+	print(color.to(color_format))

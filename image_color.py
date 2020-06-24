23d7da00b86449bd185da55e47871d7d9b397770
César Salcedo <cesar.salcedo@utec.edu.pe>
2020-06-24 16:51:36 -0500

    Updated implementation of image full color choosing file

--- image_color.py
+++ image_color.py
@@ -4,8 +4,8 @@ Find LAB color of image from IMAGE_PATH.
 
 import sys
 
-if len(sys.argv) != 2:
-	print("Usage: python3 image_lab.py <image_path>")
+if len(sys.argv) not in [2, 3]:
+	print("Usage: python3 image_color.py <image_path> [<color_format>]")
 	exit()
 
 import cv2
@@ -16,8 +16,18 @@ from functions.utils import full_image_contour
 from functions.main import get_colors
 from functions import choose_valid_points
 
+# Arguments
+if len(sys.argv) == 3:
+	if sys.argv[2] not in ["RGB", "BGR", "LAB"]:
+		raise Exception("Unsupported color format " + str(sys.argv[2]))
+	
+	color_format = sys.argv[2]
+else:
+	color_format = "LAB"
+
 image_path = sys.argv[1]
 
+# Program
 image = cv2.imread(image_path)
 
 contour = full_image_contour(image)
@@ -27,5 +37,4 @@ colors = get_colors(image, [contour],
 	filter_out_of_range=False
 )["object_colors"]
 
-print("LAB color of image")
-print(color.rgb2lab(colors[0]["color"][::-1] / 255))
+print(colors[0].to(color_format))

6d8d088cb9e34001448e44f5ffbbae3c72f76d86
César Salcedo <cesar.salcedo@utec.edu.pe>
2020-06-30 19:13:57 -0500

    Implemented CLI for image_object_detection.py

--- image_object_color.py
+++ image_object_color.py
@@ -3,33 +3,27 @@ Load image from IMAGE_PATH, detect the objects in it and print their colors
 as COLOR_FORMAT.
 """
 
-import sys
-
-if len(sys.argv) not in [2, 3, 4]:
-	raise Exception("Usage: python3 image_object_color.py <image_path> [<color_format>]")
+import click
 
 import cv2
 from functions.main import *
 
+@click.command()
+@click.argument('image_path', type=click.Path(exists=True))
+@click.option('--format', '-f', "color_format",
+	type=click.Choice(["RGB", "BGR", "LAB"],
+	case_sensitive=False),
+	default="LAB"
+)
+def main(image_path, color_format):
+	image = cv2.imread(image_path)
+	
+	contours = detect_objects(image)
+	colors = get_colors(image, contours, choose_color = choose_color.biggest_colored_cluster)["object_colors"]
+	
+	for color in colors:
+		print(color.to(color_format))
 
-# Arguments
-## Image path
-image_path = sys.argv[1]
 
-## Color format
-if len(sys.argv) == 3:
-	if sys.argv[2] not in ["RGB", "BGR", "LAB"]:
-		raise Exception("Unsupported color format " + str(sys.argv[2]))
-
-	color_format = sys.argv[2]
-else:
-	color_format = "LAB"
-
-
-image = cv2.imread(image_path)
-
-contours = detect_objects(image)
-colors = get_colors(image, contours, choose_color = choose_color.biggest_colored_cluster)["object_colors"]
-
-for color in colors:
-	print(color.to(color_format))
+if __name__ == "__main__":
+	print(main())

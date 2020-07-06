47d6be5f74daa898e5334061de8feb181a02eb0e
César Salcedo <cesar.salcedo@utec.edu.pe>
2020-07-06 17:42:20 -0500

    Implemented CLI for image_reference_recoloring.py

--- image_reference_recoloring.py
+++ image_reference_recoloring.py
@@ -3,32 +3,29 @@ Use image from REFERENCE_IMAGE_PATH to adjust color of image
 from TARGET_IMAGE_PATH. The result image is saved in RESULT_PATH
 """
 
-import sys
-
-if len(sys.argv) not in [3, 4]:
-	print("Usage: python3 image_reference_recoloring.py <reference_image_path> <target_image_path> [<result_path>]")
-	exit()
+import click
 
 import cv2
 from functions.main import *
 
-reference_image_path = sys.argv[1]
-target_image_path = sys.argv[2]
+@click.command()
+@click.argument('reference_image_path', type=click.Path(exists=True))
+@click.argument('target_image_path', type=click.Path(exists=True))
+@click.option('--result-path', '-r', type=click.Path(), default="result.jpeg")
+def main(reference_image_path, target_image_path, result_path):
+	reference_image = cv2.imread(reference_image_path)
+	target_image = cv2.imread(target_image_path)
+	
+	reference_contours = detect_objects(reference_image)
+	reference_colors = get_colors(reference_image, reference_contours)["image_colors"]
+	
+	target_contours = detect_objects(target_image)
+	target_colors = get_colors(target_image, target_contours)["image_colors"]
+	
+	recolored_image = image_recoloring(target_image, target_colors, reference_colors)
+	
+	cv2.imwrite(result_path, recolored_image)
 
-if len(sys.argv) == 4:
-	result_path = sys.argv[3]
-else:
-	result_path = "result.png"
 
-reference_image = cv2.imread(reference_image_path)
-target_image = cv2.imread(target_image_path)
-
-reference_contours = detect_objects(reference_image)
-reference_colors = get_colors(reference_image, reference_contours)["image_colors"]
-
-target_contours = detect_objects(target_image)
-target_colors = get_colors(target_image, target_contours)["image_colors"]
-
-recolored_image = image_recoloring(target_image, target_colors, reference_colors)
-
-cv2.imwrite(result_path, recolored_image)
+if __name__ == "__main__":
+	main()

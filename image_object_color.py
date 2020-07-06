54d35b657a104b6d40699be2a6e5761dbd335324
César Salcedo <cesar.salcedo@utec.edu.pe>
2020-07-06 17:46:20 -0500

    image_object_color.py supports flags that enable stats printing and image midification

--- image_object_color.py
+++ image_object_color.py
@@ -13,16 +13,35 @@ from functions.main import *
 @click.option('--format', '-f', "color_format",
 	type=click.Choice(["RGB", "BGR", "LAB"],
 	case_sensitive=False),
-	default="LAB"
+	default="LAB",
+	show_default=True
 )
-def main(image_path, color_format):
+@click.option('--draw-box', '-b', is_flag=True)
+@click.option('--draw-points', '-p', is_flag=True)
+@click.option('--print-stats', '-s', is_flag=True)
+def main(image_path, color_format, draw_box, draw_points, print_stats):
 	image = cv2.imread(image_path)
+
+	stats_format = None
+
+	if print_stats:
+		stats_format = color_format
 	
 	contours = detect_objects(image)
-	colors = get_colors(image, contours, choose_color = choose_color.biggest_colored_cluster)["object_colors"]
+	colors = get_colors(image, contours,
+		choose_color=choose_color.biggest_colored_cluster,
+		draw_box=draw_box,
+		draw_points=draw_points,
+		stats_format=stats_format
+	)["object_colors"]
 	
-	for color in colors:
-		print(color.to(color_format))
+	if not print_stats:
+		for color in colors:
+			print(color.to(color_format))
+	
+	if draw_box or draw_points:
+		cv2.imshow('result', image)
+		cv2.waitKey(0)
 
 
 if __name__ == "__main__":
